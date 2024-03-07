9fbe06351e5635273a0b9e23204936ac444deede
Luthfi Khaeri Ihsan <105151516+Luthfikhaerii@users.noreply.github.com>
2024-03-07 14:20:43 +0700

    Update file_controller.js

--- controllers/file_controller.js
+++ controllers/file_controller.js
@@ -45,7 +45,7 @@ const invoice_controller = {
             const pesan = await jp_function()
 
             if (pesan) {
-                const templateFile = fs.readFileSync(__dirname+'../templates/invoice.docx');
+                const templateFile = fs.readFileSync(path.join(__dirname,'../templates/invoice.docx'));
 
                 // 2. process the template
                 const data = {
@@ -108,7 +108,7 @@ const invoice_controller = {
            
             if (deskripsi) {
           
-                fs.readFile(path.join(__dirname+'../templates/bon.xlsx'), function (err, data) {
+                fs.readFile(path.join(__dirname,'../templates/bon.xlsx'), function (err, data) {
                     
                     // Create a template
                     var template = new XlsxTemplate(data);
