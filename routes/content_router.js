be2b22e60b5f961ca86be186597de8803b90c19b
luthfikhaerii <luthfikhaeri00@gmail.com>
2024-06-07 16:27:09 +0700

    update middleware

--- routes/content_router.js
+++ routes/content_router.js
@@ -5,9 +5,9 @@ const {auth,auth_admin} = require('../middlewares/auth')
 
 router.post("/content",multer().any(),content_controller.add_content)
 router.delete("/content/:id",auth,auth_admin,content_controller.delete_content)
-router.put("/content/:id",auth,auth_admin,content_controller.update_content)
-router.put("/content_foto/:id",auth,auth_admin,multer().single('foto'),content_controller.update_foto)
-router.put("/content_contoh_hasil/:id",auth,auth_admin,multer().single('contoh_hasil'),content_controller.update_contoh_hasil)
+router.put("/content/:id",content_controller.update_content)
+router.put("/content_foto/:id",multer().single('foto'),content_controller.update_foto)
+router.put("/content_contoh_hasil/:id",multer().single('contoh_hasil'),content_controller.update_contoh_hasil)
 router.get("/content/:id?",content_controller.get_content)
 
 module.exports = router
