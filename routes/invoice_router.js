57cff78b88229db168f3de2c6a7c8ba6de0ffcfc
luthfikhaerii <luthfikhaeri00@gmail.com>
2024-06-07 16:25:52 +0700

    update

--- routes/invoice_router.js
+++ routes/invoice_router.js
@@ -3,7 +3,7 @@ const invoice_controller = require("../controllers/invoice_controller")
 const {auth} = require('../middlewares/auth')
 
 router.get("/invoice/:id?",invoice_controller.get_invoice)
-router.put("/invoice/:id",auth,invoice_controller.update_invoice)
-router.delete("/invoice",auth,invoice_controller.delete_invoice)
+router.put("/invoice/:id",invoice_controller.update_invoice)
+router.delete("/invoice",invoice_controller.delete_invoice)
 
 module.exports = router
--- routes/order_router.js
+++ routes/order_router.js
@@ -4,8 +4,8 @@ const {auth} = require('../middlewares/auth')
 const multer = require("multer")
 
 router.get("/order/:id?",order_controller.get_order)
-router.post("/order",auth,multer().any(),order_controller.add_order)
-router.put("/order/:id",auth,order_controller.update_order)
-router.delete("/order/:id",auth,order_controller.delete_order)
+router.post("/order",multer().any(),order_controller.add_order)
+router.put("/order/:id",order_controller.update_order)
+router.delete("/order/:id",order_controller.delete_order)
 
 module.exports = router
