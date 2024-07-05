970c77d14b2b14007625387c58c9f7cec0ea9943
Fikri Zaki Aditama, S.Si <fikriyomann@student.upi.edu>
2024-07-05 09:12:51 +0700

    harga satuan ilang

--- controllers/file_controller.js
+++ controllers/file_controller.js
@@ -33,7 +33,7 @@ const invoice_controller = {
                     obj.jumlah += 1
                     list_jp.push(v.jenis_pengujian)
                     obj.jumlah = v.jumlah_sample
-                    obj.hs = ((v.total_harga / v.jumlah_sample).toLocaleString('id-ID', { style: 'currency', currency: 'IDR', minimumFractionDigits: 0 })).replace(/\bRp\b/g, "");
+                    //obj.hs = ((v.total_harga / v.jumlah_sample).toLocaleString('id-ID', { style: 'currency', currency: 'IDR', minimumFractionDigits: 0 })).replace(/\bRp\b/g, "");
                     obj.jb = (v.total_harga.toLocaleString('id-ID', { style: 'currency', currency: 'IDR', minimumFractionDigits: 0 })).replace(/\bRp\b/g, "");
                     data_pesan.push(obj)
                 })
@@ -262,10 +262,10 @@ const invoice_controller = {
                 console.log(task);
                 await Invoice.updateOne({ _id: invoice_id }, { opTask: true })
                 res.send("success");
-            }else{
+            } else {
                 res.send("success");
             }
-           
+
         } catch (err) {
             res.status(500).json({
                 success: false,
