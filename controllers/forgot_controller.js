7f0c1f25d708d6471435efc41ea34283c2d3e9de
luthfikhaerii <luthfikhaeri00@gmail.com>
2024-07-25 21:15:10 +0700

    update

---
+++ controllers/forgot_controller.js
@@ -0,0 +1,13 @@
+const forgot_controller = {
+  send_email : async (req,res)=>{
+    res.send('sukses')
+  },
+  verify_token : async (req,res)=>{
+    res.send('sukses')
+  },
+  update_password : async(req,res)=>{
+    res.send('sukses')
+  }
+}
+
+module.exports = forgot_controller
--- routes/router.js
+++ routes/router.js
@@ -4,5 +4,6 @@ router.use("/",require("./order_router"))
 router.use("/",require("./invoice_router"))
 router.use("/",require("./file_router"))
 router.use("/",require("./content_router"))
+router.use("/",require("./forgot_router"))
 
 module.exports = router
