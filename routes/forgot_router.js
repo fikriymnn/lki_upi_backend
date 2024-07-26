75d0b6c21c4311afd0a227d23b19771f292adfed
luthfikhaerii <luthfikhaeri00@gmail.com>
2024-07-26 12:38:28 +0700

    update

--- routes/forgot_router.js
+++ routes/forgot_router.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 
 const forgot_controller = require('../controllers/forgot_controller')
-router.get('/lupaPassword/:email',forgot_controller.sendEmail)
+router.get('/lupaPassword/:email',forgot_controller.send_email)
 
 module.exports = router
