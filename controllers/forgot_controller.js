778c4347c37c14f60cbd98c8182ad293e7926158
luthfikhaerii <luthfikhaeri00@gmail.com>
2024-07-30 16:53:47 +0700

    update

--- controllers/forgot_controller.js
+++ controllers/forgot_controller.js
@@ -1,12 +1,90 @@
+const Users = require('../model/user_model')
+const {generate_access_token} = require('../utils/jwt')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+
 const forgot_controller = {
-  send_email : async (req,res)=>{
-    res.send('sukses')
+  send_email: async (req, res) => {
+    try{
+    const {email} = req.params
+
+    if(!email){
+      res.status(400).json({message: "Email tidak valid"})
+    }
+    
+    const data = await Users.findOne({email})
+    if(!data){
+      res.status(400).json({message: "Email tidak valid"})
+    }
+    
+    const email_encrypt = generate_access_token({email})
+    
+
+
+    const nodemailer = require('nodemailer');
+
+    // Konfigurasi transport
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'luthfikhaeri00@gmail.com',
+        pass: 'your_password'
+      }
+    });
+
+    // Opsi email
+    const mailOptions = {
+      from: 'luthfikhaeri00@gmail.com',
+      to: email,
+      subject: 'Verifikasi Email LKI UPI',
+      html: `<a href="https://www.lablkiupi.com/updatePassword/${email_encrypt}>Klik Untuk Ubah Password</a>`
+    };
+
+    // Kirim email
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+    res.status(200).json({message:"success",success:true})
+  }catch(err){
+    res.status(500).json({message:err.message,success:false})
+  }
   },
-  verify_token : async (req,res)=>{
-    res.send('sukses')
+  verify_token: async (req, res) => {
+    try{
+        const {token} = req.params
+        if(!token){
+          res.status(400).json({message: "Token tidak valid"})
+        }
+        const payload  = jwt.verify(token,process.env.ACC_TOKEN_SECRET)
+        console.log(payload)
+        res.status(200).json({message:"success",success:true})
+    }catch(err){
+      res.status(500).json({message:err.message,success:false})
+    }
   },
-  update_password : async(req,res)=>{
-    res.send('sukses')
+  update_password: async (req, res) => {
+    try{
+      const {email} = req.params
+      const {password} = req.body
+
+      if(!email){
+        res.status(400).json({message: "Email tidak valid"})
+      }
+
+      if(!password){
+        res.status(400).json({message: "Password tidak kosong"})
+      }
+      const new_password = await bcrypt.hash(password, 10)
+      await Users.updateOne({email},{password:new_password})
+      
+      res.status(200).json({message:"success",success:true})
+    }catch(err){
+      res.status(500).json({message:err.message,success:false})
+    }
   }
 }
 
--- routes/forgot_router.js
+++ routes/forgot_router.js
@@ -2,5 +2,7 @@ const router = require('express').Router()
 
 const forgot_controller = require('../controllers/forgot_controller')
 router.get('/lupaPassword/:email',forgot_controller.send_email)
+router.get('/verifyToken/:token',forgot_controller.verify_token)
+router.put('/lupaPassword/:email',forgot_controller.update_password)
 
 module.exports = router
