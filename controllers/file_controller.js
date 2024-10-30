6a0daba286575550d6dc44b6ec572f71db2bb9a7
Fikri Zaki Aditama, S.Si. <fikriyomann@gmail.com>
2024-10-30 11:57:32 +0700

    Update file_controller.js

--- controllers/file_controller.js
+++ controllers/file_controller.js
@@ -10,7 +10,7 @@ const fs = require("fs");
 const path = require("path");
 const replaceTextInPDF = require("../utils/pdfreplace.js");
 const angkaketext = require("../utils/angkatotext.js");
-var convertapi = require("convertapi")("secret_d59OXaMGR3zXYF2O");
+var convertapi = require("convertapi")("secret_OYIUzRZjUouYysQk");
 
 const invoice_controller = {
     get_invoice: async (req, res) => {
