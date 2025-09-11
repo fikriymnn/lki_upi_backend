304e4df75042d33b1d4d2070b476da36382acde1
Luthfi Khaeri Ihsan <105151516+Luthfikhaerii@users.noreply.github.com>
2025-09-11 09:58:49 +0700

    Update file_controller.js

--- controllers/file_controller.js
+++ controllers/file_controller.js
@@ -10,7 +10,7 @@ const fs = require("fs");
 const path = require("path");
 const replaceTextInPDF = require("../utils/pdfreplace.js");
 const angkaketext = require("../utils/angkatotext.js");
-var convertapi = require("convertapi")("AqZntuhNG3hEvBcc7QjZI2KBeP3FbgxW");
+var convertapi = require("convertapi")("nDCg6GHcZox2ydGclndGvyiRrEKn9V3o");
 
 const invoice_controller = {
     get_invoice: async (req, res) => {
