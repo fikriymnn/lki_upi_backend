3bc209d702848ddd3676b866ffa472ce8d351f01
luthfikhaerii <luthfikhaeri00@gmail.com>
2026-03-11 10:44:42 +0700

    update invoice template file

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -7,51 +7,52 @@ const user_controller = {
       try {
          const body = req.body
 
-        
+
          const user_exist = await User.findOne({ email: body.email })
          if (user_exist) {
             console.log(0)
-           return res.status(200).json({
+            return res.status(200).json({
                status: 400,
                message: "Email telah digunakan."
             })
          }
- 
+
          const hash_password = await bcrypt.hash(body.password, 10)
-         const new_user = new User({ 
-            email:body.email,
+         const new_user = new User({
+            email: body.email,
             password: hash_password,
-            nama_lengkap: body.nama_lengkap, 
+            nama_lengkap: body.nama_lengkap,
             nama_institusi: body.nama_institusi,
             jenis_institusi: body.jenis_institusi,
             no_telp: body.no_telp,
-            no_whatsapp:body.no_whatsapp,
-            program_studi:body.program_studi,
-            fakultas:body.fakultas})
+            no_whatsapp: body.no_whatsapp,
+            program_studi: body.program_studi,
+            fakultas: body.fakultas
+         })
          await new_user.save()
 
-         const user = await User.findOne({ email:body.email })
-         const access_token = generate_access_token({ _id: user._id, email: body.email, role: user.role, jenis_institusi: body.jenis_institusi, nama_institusi: body.nama_institusi, no_telp: body.no_telp, no_whatsapp: user.no_whatsapp,nama_lengkap: body.nama_lengkap })
-const millisecondsInDay = 1000 * 60 * 60 * 24;
-  const expiresInMilliseconds = millisecondsInDay * 999;
- 
+         const user = await User.findOne({ email: body.email })
+         const access_token = generate_access_token({ _id: user._id, email: body.email, role: user.role, jenis_institusi: body.jenis_institusi, nama_institusi: body.nama_institusi, no_telp: body.no_telp, no_whatsapp: user.no_whatsapp, nama_lengkap: body.nama_lengkap })
+         const millisecondsInDay = 1000 * 60 * 60 * 24;
+         const expiresInMilliseconds = millisecondsInDay * 999;
+
          res.cookie("access_token", access_token, {
             httpOnly: true,
-            path:"/",
-            sameSite:'None', 
+            path: "/",
+            sameSite: 'None',
             secure: true,
-            expires:new Date(Date.now() + expiresInMilliseconds)
+            expires: new Date(Date.now() + expiresInMilliseconds)
          })
-        
+
 
          return res.status(200).json({
             success: true,
-            token:access_token,
+            token: access_token,
             data: {
-               _id: user._id, email: body.email, role: user.role, jenis_institusi: body.jenis_institusi, nama_institusi: body.nama_institusi, no_telp: body.no_telp, nama_lengkap: body.nama_lengkap,no_whatsapp: user.no_whatsapp
+               _id: user._id, email: body.email, role: user.role, jenis_institusi: body.jenis_institusi, nama_institusi: body.nama_institusi, no_telp: body.no_telp, nama_lengkap: body.nama_lengkap, no_whatsapp: user.no_whatsapp
             }
          })
-        
+
 
       } catch (err) {
          console.log(err.message)
@@ -67,16 +68,16 @@ const millisecondsInDay = 1000 * 60 * 60 * 24;
          console.log(0)
          if (req.cookies.access_token) {
             return res.status(200).json({
-               success:true,
-                status: 200,
-                message: "Login successfully"
-             })
-          }
+               success: true,
+               status: 200,
+               message: "Login successfully"
+            })
+         }
          const { password, email } = req.body
          if (!password && !email) {
-           return res.status(200).json({
+            return res.status(200).json({
                status: 400,
-               success:false,
+               success: false,
                message: "email atau password salah."
             })
          }
@@ -86,7 +87,7 @@ const millisecondsInDay = 1000 * 60 * 60 * 24;
          if (!user) {
             return res.status(200).json({
                status: 400,
-               success:false,
+               success: false,
                message: "email tidak ditemukan."
             })
          }
@@ -95,31 +96,31 @@ const millisecondsInDay = 1000 * 60 * 60 * 24;
          if (!compare) {
             return res.status(200).json({
                status: 400,
-               success:false,
+               success: false,
                message: "password salah."
             })
          }
 
          const access_token = generate_access_token({
-            _id: user._id, email: user.email, role: user.role, jenis_institusi: user.jenis_institusi, nama_institusi: user.nama_institusi, no_telp: user.no_telp, nama_lengkap: user.nama_lengkap,no_whatsapp: user.no_whatsapp
+            _id: user._id, email: user.email, role: user.role, jenis_institusi: user.jenis_institusi, nama_institusi: user.nama_institusi, no_telp: user.no_telp, nama_lengkap: user.nama_lengkap, no_whatsapp: user.no_whatsapp
          })
 
          const millisecondsInDay = 1000 * 60 * 60 * 24;
-  const expiresInMilliseconds = millisecondsInDay * 999;
- 
+         const expiresInMilliseconds = millisecondsInDay * 999;
+
          res.cookie("access_token", access_token, {
             httpOnly: true,
-            path:"/",
-            sameSite:'None', 
+            path: "/",
+            sameSite: 'None',
             secure: true,
-            expires:new Date(Date.now() + expiresInMilliseconds)
+            expires: new Date(Date.now() + expiresInMilliseconds)
          })
 
          return res.status(200).json({
             success: true,
-            token:access_token,
+            token: access_token,
             data: {
-               _id: user._id, email: user.email, role: user.role, jenis_institusi: user.jenis_institusi, nama_institusi: user.nama_institusi, no_telp: user.no_telp, nama_lengkap: user.nama_lengkap,no_whatsapp: user.no_whatsapp,
+               _id: user._id, email: user.email, role: user.role, jenis_institusi: user.jenis_institusi, nama_institusi: user.nama_institusi, no_telp: user.no_telp, nama_lengkap: user.nama_lengkap, no_whatsapp: user.no_whatsapp,
             }
          })
       } catch (err) {
@@ -130,22 +131,22 @@ const millisecondsInDay = 1000 * 60 * 60 * 24;
          })
       }
    },
-   get_user: async (req,res) => {
+   get_user: async (req, res) => {
       try {
-        
+
          const data = req.user
-         if(!data){
+         if (!data) {
             res.status(200).json({
                success: false,
                data: "user is not exist"
             })
-         }else{
+         } else {
             return res.status(200).json({
                success: true,
                data
             })
          }
-         
+
       } catch (err) {
          return res.status(500).json({
             success: false,
@@ -153,17 +154,17 @@ const millisecondsInDay = 1000 * 60 * 60 * 24;
          })
       }
    },
-   getAdmin_user: async (req,res) => {
+   getAdmin_user: async (req, res) => {
       try {
-        
-         const data = await User.findOne({role:'admin'})
-         const dataOp = await User.findOne({role:'operator'})
-         const dataPj = await User.findOne({role:'pj'})
-      
+
+         const data = await User.findOne({ role: 'admin' })
+         const dataOp = await User.findOne({ role: 'operator' })
+         const dataPj = await User.findOne({ role: 'pj' })
+
          res.status(200).json({
             success: true,
             data: {
-               admin:data,operator:dataOp,pj:dataPj
+               admin: data, operator: dataOp, pj: dataPj
             }
          })
       } catch (err) {
@@ -173,16 +174,18 @@ const millisecondsInDay = 1000 * 60 * 60 * 24;
          })
       }
    },
-   logout: async (req,res) => {
+   logout: async (req, res) => {
       try {
-         if(!req.cookies.access_token){
+         if (!req.cookies.access_token) {
             return res.status(200).json({
                success: false,
                message: "Logout failed!"
             })
          }
-         res.clearCookie('access_token',{ sameSite:'None',
-         secure: true,})
+         res.clearCookie('access_token', {
+            sameSite: 'None',
+            secure: true,
+         })
 
          return res.status(200).json({
             success: true,
@@ -195,89 +198,89 @@ const millisecondsInDay = 1000 * 60 * 60 * 24;
          })
       }
    },
-   update_user: async(req,res)=>{
-      try{
+   update_user: async (req, res) => {
+      try {
          const body = req.body
-         const {id} = req.params
-         await User.updateOne({_id:id},body)
-         const user = await User.findOne({_id:id})
+         const { id } = req.params
+         await User.updateOne({ _id: id }, body)
+         const user = await User.findOne({ _id: id })
          const access_token = generate_access_token({
-            _id: user._id, email: user.email, role: user.role, jenis_institusi: user.jenis_institusi, nama_institusi: user.nama_institusi, no_telp: user.no_telp, nama_lengkap: user.nama_lengkap,no_whatsapp: user.no_whatsapp
+            _id: user._id, email: user.email, role: user.role, jenis_institusi: user.jenis_institusi, nama_institusi: user.nama_institusi, no_telp: user.no_telp, nama_lengkap: user.nama_lengkap, no_whatsapp: user.no_whatsapp
          })
          const millisecondsInDay = 1000 * 60 * 60 * 24;
-  const expiresInMilliseconds = millisecondsInDay * 999;
- 
+         const expiresInMilliseconds = millisecondsInDay * 999;
+
          res.cookie("access_token", access_token, {
             httpOnly: true,
-            path:"/",
-            sameSite:'None', 
+            path: "/",
+            sameSite: 'None',
             secure: true,
-            expires:new Date(Date.now() + expiresInMilliseconds)
+            expires: new Date(Date.now() + expiresInMilliseconds)
          })
          return res.status(200).json({
             success: true,
             data: 'update successfully'
          })
 
-      }catch(err){
+      } catch (err) {
          return res.status(500).json({
             success: false,
             message: err.message
          })
       }
    },
-   edit_user: async(req,res)=>{
-      try{
+   edit_user: async (req, res) => {
+      try {
          const body = req.body
-         const {password} = req.body
-         const {id} = req.params
-         if(password){
+         const { password } = req.body
+         const { id } = req.params
+         if (password) {
             const hash_password = await bcrypt.hash(password, 10)
             body.password = hash_password
-            await User.updateOne({_id:id},body)
-         }else{
-            await User.updateOne({_id:id},body)
-         }     
+            await User.updateOne({ _id: id }, body)
+         } else {
+            await User.updateOne({ _id: id }, body)
+         }
          return res.status(200).json({
             success: true,
             data: 'update successfully'
          })
-      }catch(err){
+      } catch (err) {
          return res.status(500).json({
             success: false,
             message: err.message
          })
       }
-   }, remember_user: async (req,res) => {
+   }, remember_user: async (req, res) => {
       try {
-        
+
          const data = req.user
-         const user= req.user
-         if(!data){
+         const user = req.user
+         if (!data) {
             res.status(200).json({
                success: false,
                data: "user is not exist"
             })
-         }else{
+         } else {
             const access_token = generate_access_token({
-               _id: user._id, email: user.email, role: user.role, jenis_institusi: user.jenis_institusi, nama_institusi: user.nama_institusi, no_telp: user.no_telp, nama_lengkap: user.nama_lengkap,no_whatsapp: user.no_whatsapp
+               _id: user._id, email: user.email, role: user.role, jenis_institusi: user.jenis_institusi, nama_institusi: user.nama_institusi, no_telp: user.no_telp, nama_lengkap: user.nama_lengkap, no_whatsapp: user.no_whatsapp
             })
             const millisecondsInDay = 1000 * 60 * 60 * 24;
-  const expiresInMilliseconds = millisecondsInDay * 999;
- 
-         res.cookie("access_token", access_token, {
-            httpOnly: true,
-            path:"/",
-            sameSite:'None', 
-            secure: true,
-            expires:new Date(Date.now() + expiresInMilliseconds)
-         })
+            const expiresInMilliseconds = millisecondsInDay * 999;
+
+            res.cookie("access_token", access_token, {
+               httpOnly: true,
+               path: "/",
+               sameSite: 'None',
+               secure: true,
+               expires: new Date(Date.now() + expiresInMilliseconds)
+            })
             return res.status(200).json({
                success: true,
                data
             })
          }
-         
+
       } catch (err) {
          return res.status(500).json({
             success: false,
