e2e2833f373d7cac7bcad8af2b9efe89e7f99dc8
luthfikhaerii <luthfikhaeri00@gmail.com>
2026-05-12 03:23:09 +0700

    update

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,20 +1,24 @@
 const jwt = require("jsonwebtoken")
 
 const auth_middleware = {
-    auth: (req,res,next)=>{
-        try{
-            const token = req.cookies.access_token || req.params.id
-            if(!token){
+    auth: (req, res, next) => {
+        try {
+            const token = req.cookies.access_token
+                || req.params.id
+                || req.headers.authorization?.replace('Bearer ', '')
+
+            if (!token) {
                 return res.status(401).json({
-                    success:false,
+                    success: false,
                     status_code: 401,
                     message: "Access token is not exist."
                 })
             }
-            jwt.verify(token,process.env.ACC_TOKEN_SECRET,(err,payload)=>{
-                if(err){
+
+            jwt.verify(token, process.env.ACC_TOKEN_SECRET, (err, payload) => {
+                if (err) {
                     return res.status(403).json({
-                        success:false,
+                        success: false,
                         status_code: 403,
                         message: "Access token invalid."
                     })
@@ -22,67 +26,66 @@ const auth_middleware = {
                 req.user = payload
                 next()
             })
-        }catch(err){
+        } catch (err) {
             return res.status(500).json({
-                success:false,
+                success: false,
                 status_code: 500,
-                message:err.message
-
+                message: err.message
             })
         }
     },
-    auth_admin:(req,res,next)=>{
-        try{
+    auth_admin: (req, res, next) => {
+        try {
             const user = req.user
-            if(!user){
-                return  res.status(403).json({
-                    success:false,
-                    status_code: 403,
-                    message: "Access token invalid."
-                })
-            }
-            if(user.role!=="admin"){
-                return res.status(403).json({
-                    success:false,
-                    status_code: 403,
-                    message: "You are not permitted to access"
-                })
-            }
-
-            next()
-        }catch(err){
-            return res.status(500).json({
-                success:false,
-                status_code: 500,
-                message:err.message
-
-            })
-        }
-    },
-    auth_operator:(req,res,next)=>{
-        try{
-            const user = req.user
-            if(!user){
+            if (!user) {
                 return res.status(403).json({
                     success: false,
                     status_code: 403,
                     message: "Access token invalid."
                 })
             }
-            if(user.role!=="operator"){
+            if (user.role !== "admin") {
                 return res.status(403).json({
-                    success:false,
+                    success: false,
                     status_code: 403,
                     message: "You are not permitted to access"
                 })
             }
 
             next()
-        }catch(err){
+        } catch (err) {
             return res.status(500).json({
-                success:false,
+                success: false,
                 status_code: 500,
-                message:err.message
+                message: err.message
+
+            })
+        }
+    },
+    auth_operator: (req, res, next) => {
+        try {
+            const user = req.user
+            if (!user) {
+                return res.status(403).json({
+                    success: false,
+                    status_code: 403,
+                    message: "Access token invalid."
+                })
+            }
+            if (user.role !== "operator") {
+                return res.status(403).json({
+                    success: false,
+                    status_code: 403,
+                    message: "You are not permitted to access"
+                })
+            }
+
+            next()
+        } catch (err) {
+            return res.status(500).json({
+                success: false,
+                status_code: 500,
+                message: err.message
 
             })
         }
